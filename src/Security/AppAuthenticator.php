8efd53430ebee65296a99b522c56190413d91d37
evanh-shumakov <evanh.shumakov@gmail.com>
2023-09-29 03:18:33 +0400

    stat. anal. check parameters types

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\BadgeInterface;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
@@ -25,19 +26,26 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
     {
     }
 
+    /**
+     * @param Request $request
+     * @return Passport
+     *
+     * @throws \InvalidArgumentException Email required
+     */
     public function authenticate(Request $request): Passport
     {
-        $email = $request->request->get('email', '');
+        try {
+            $badges = $this->getBadges($request);
+            $credentials = $this->getPasswordCredentials($request);
+            $userBadge = $this->getUserBadge($request);
+        } catch (\InvalidArgumentException $e) {
+            throw new \InvalidArgumentException('No required argument.');
+        }
 
-        $request->getSession()->set(Security::LAST_USERNAME, $email);
+        $request->getSession()
+            ->set(Security::LAST_USERNAME, $userBadge->getUserIdentifier());
 
-        return new Passport(
-            new UserBadge($email),
-            new PasswordCredentials($request->request->get('password', '')),
-            [
-                new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
-            ]
-        );
+        return new Passport($userBadge, $credentials, $badges);
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
@@ -52,5 +60,57 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
     protected function getLoginUrl(Request $request): string
     {
         return $this->urlGenerator->generate(self::LOGIN_ROUTE);
+    }
+
+    /**
+     * @param Request $request
+     * @return BadgeInterface[]
+     *
+     * @throws \InvalidArgumentException "CSRF token required"
+     */
+    public function getBadges(Request $request): array
+    {
+        $csrfToken = $request->request->get('_csrf_token');
+
+        if (! is_string($csrfToken) || trim($csrfToken) === '') {
+            throw new \InvalidArgumentException("CSRF token required");
+        }
+
+        return [
+            new CsrfTokenBadge('authenticate', $csrfToken),
+        ];
+    }
+
+    /**
+     * @param Request $request
+     * @return UserBadge
+     *
+     * @throws \InvalidArgumentException "Email required"
+     */
+    public function getUserBadge(Request $request): UserBadge
+    {
+        $email = $request->request->get('email', '');
+
+        if(! is_string($email) || trim($email) === '') {
+            throw new \InvalidArgumentException("Email required.");
+        }
+
+        return new UserBadge($email);
+    }
+
+    /**
+     * @param Request $request
+     * @return PasswordCredentials
+     *
+     * @throws \InvalidArgumentException "Password required"
+     */
+    public function getPasswordCredentials(Request $request): PasswordCredentials
+    {
+        $password = $request->request->get('password', '');
+
+        if (! is_string($password) || trim($password) === '') {
+            throw new \InvalidArgumentException("Password required");
+        }
+        return new PasswordCredentials($password);
     }
 }
