0a8499c4a0553946db951db40862f27ff3413513
evanh-shumakov <evanh.shumakov@gmail.com>
2023-11-23 13:40:42 +0400

    add app:outage:update argument 'date'

--- src/Command/Outage/UpdateCommand.php
+++ src/Command/Outage/UpdateCommand.php
@@ -8,6 +8,7 @@ use App\Factory\Entity\NewsFactory;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
@@ -18,9 +19,18 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 )]
 class UpdateCommand extends Command
 {
-    public function __construct(
-        readonly EntityManagerInterface $entityManager,
-    ) {
+    protected function configure(): void
+    {
+        $this->addArgument(
+            'date',
+            InputArgument::OPTIONAL,
+            'A date/time string',
+            'tomorrow'
+        );
+    }
+
+    public function __construct(readonly EntityManagerInterface $entityManager)
+    {
         parent::__construct();
     }
 
